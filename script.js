eab4694c19068821c2a5b7f26c73162fa2fd9a36
Samiksha gupta <samikshagupta0299@gmail.com>
2026-06-23 02:53:44 +0530

    ui

---
+++ script.js
@@ -0,0 +1,20 @@
+ let themeswitcher = document.querySelector("#themebtn");
+
+
+
+
+const gradients = [
+    "linear-gradient(to bottom, #4CAF50, #FFFFFF)", 
+    "linear-gradient(to right, #6fb9f6, #e4e3e3)", 
+    "linear-gradient(to right, #8d8a8a, #5555da)", 
+    "linear-gradient(to right, #b0acac, #b6b6be)"  
+  ];
+
+  let current = 0; 
+  themeswitcher.addEventListener("click", function() {
+    
+    document.body.style.background = gradients[current];
+
+    
+    current = (current + 1) % gradients.length;
+  });
